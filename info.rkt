0ec44e5b8b6fb83a7889329bfb49d17e75a358df
Henrik Ravn <hravnx@gmail.com>
2018-09-30 20:25:05 +0200

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "nuggets")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/nuggets.scrbl" ())))
+(define pkg-desc "Small collection of useful helpers for script-like programming")
+(define version "0.1")
+(define pkg-authors '(hravnx))
---
+++ scribblings/nuggets.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[nuggets
+                    racket/base]]
+
+@title{nuggets}
+@author{Henrik Ravn}
+
+@defmodule[nuggets]
+
+Small utility package that provides functions to help with script-like systems programming.
