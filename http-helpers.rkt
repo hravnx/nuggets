e652329f51f428e9581949734cc4055776208246
Henrik Ravn <hravnx@gmail.com>
2018-10-02 07:09:54 +0100

    Added more tests to http helpers, added coverage to ignore list

--- http-helpers.rkt
+++ http-helpers.rkt
@@ -14,8 +14,11 @@
 (define (get-headers url) (http-request url head-impure-port))
 
 (define (checked-string->number ctx s)
-  (cond [(string? s) (string->number s)]
-        [else (error (format "Bad string ~s in ~s" s ctx))]))
+  (let ([n (cond [(string? s) (string->number s)]
+                 [else (error (format "Non-string ~s in ~s" s ctx))])])
+    (if n
+        n
+        (error (format "String ~s is not a number in ~s" s ctx)))))
 
 (define (get-request-result-code header-text)
   (~>> header-text
@@ -28,3 +31,14 @@
        get-request-result-code
        (= 404)))
 
+(module+ test
+  (require rackunit)
+
+  (check-equal? (get-request-result-code "HTTP/1.1 200 OK") 200)
+  (check-equal? (get-request-result-code "HTTP/1.1 404 Not found") 404)
+  (check-equal? (get-request-result-code "HTTP/1.1 500 Internal Server Error") 500)
+
+  (check-equal? (checked-string->number "" "42") 42)
+  (check-exn #rx"String \"B\" is not a number in \"A\"" (λ () (checked-string->number "A" "B")))
+  (check-exn #rx"Non-string \\(\\) in \"A\"" (λ () (checked-string->number "A" '())))
+)
