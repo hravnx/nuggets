0fef3a061def0ef73e920c23d6b8086177d70890
Henrik Ravn <hravnx@gmail.com>
2018-10-02 12:42:28 +0100

    Added more tests to http helpers, coverage at 100%

--- http-helpers.rkt
+++ http-helpers.rkt
@@ -6,12 +6,17 @@
          threading
          nuggets/regex-helpers)
 
+(define GET-PROC (make-parameter get-pure-port))
+(define HEAD-PROC (make-parameter head-impure-port))
+
 (define (http-request url handler)
   (call/input-url (string->url url) handler port->string))
 
-(define (fetch url) (http-request url get-pure-port))
+(define (fetch url)
+  (http-request url (GET-PROC)))
 
-(define (get-headers url) (http-request url head-impure-port))
+(define (get-headers url)
+  (http-request url (HEAD-PROC)))
 
 (define (checked-string->number ctx s)
   (let ([n (cond [(string? s) (string->number s)]
@@ -33,6 +38,15 @@
 
 (module+ test
   (require rackunit)
+
+  (parameterize ([GET-PROC (λ (u) (open-input-string "Hello world"))])
+    (check-equal? (fetch "https://google.com") "Hello world"))
+
+  (parameterize ([HEAD-PROC (λ (u) (open-input-string "HTTP/1.1 200 OK"))])
+    (check-equal? (get-headers "https://google.com") "HTTP/1.1 200 OK"))
+
+  (parameterize ([HEAD-PROC (λ (u) (open-input-string "HTTP/1.1 404 Not Found"))])
+    (check-true (dead-link? "https://google.com")))
 
   (check-equal? (get-request-result-code "HTTP/1.1 200 OK") 200)
   (check-equal? (get-request-result-code "HTTP/1.1 404 Not found") 404)
