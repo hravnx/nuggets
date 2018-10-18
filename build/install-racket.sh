241454178d1e8fc8d20ab142f44fe233cf09afc3
Henrik Ravn <hravnx@gmail.com>
2018-10-18 11:08:12 -0700

    Raco test

--- build/install-racket.sh
+++ build/install-racket.sh
@@ -1,4 +1,4 @@
-URL="https://mirror.racket-lang.org/installers/$racketVs/racket-$racketVs-x86_64-linux.sh"
+URL="https://mirror.racket-lang.org/installers/$RACKET_VS/racket-$RACKET_VS-x86_64-linux.sh"
 INSTALLER=./install-racket-binaries.sh
 RACKET_DIR=./racket
 
@@ -17,6 +17,9 @@ no
 EOF
 
 echo "Racket installed"
+
+raco --version
+
 
 
 
