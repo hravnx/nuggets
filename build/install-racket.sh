3d16c859d5e8a02f88ad9f4b0d54faf280a52844
Henrik Ravn <hravnx@gmail.com>
2018-10-18 04:46:21 -0700

    Added actual Racket install steps

--- build/install-racket.sh
+++ build/install-racket.sh
@@ -1,1 +1,18 @@
-echo "Installing Racket ..."
+URL="https://mirror.racket-lang.org/installers/$racketVs/racket-$racketVs-x86_64-linux.sh"
+INSTALLER=install-racket.sh
+RACKET_DIR=./racket
+
+echo "Downloading Racket ..."
+curl -L -o $INSTALLER $URL
+
+echo "Making racket executable"
+chmod u+rx "$INSTALLER"
+
+echo "Running installer"
+$INSTALLER <<EOF
+no
+"$RACKET_DIR"
+EOF
+
+echo "Racket installed"
+
